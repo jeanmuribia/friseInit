f911bd80dd0f2e8a6362449304e00a1fba41e708
Jean Muribia <115085099+jeanmuribia@users.noreply.github.com>
2024-04-18 18:10:19 +0200

    avec UUID

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCorners,
 import { Column } from './components/Column/Column';
 import { arrayMove, sortableKeyboardCoordinates } from '@dnd-kit/sortable';
 import Input from './components/Input/Input';
+import { v4 as uuidv4 } from 'uuid'
 
 export default function App() {
-    const [cards, setCards] = useState([
-      { id: 1, color: '#5cdb95', title: 'Carte 1' },
-      { id: 2, color: '#f56991', title: 'Carte 2' },
-      { id: 3, color: '#f39c12', title: 'Carte 3' },
-      { id: 4, color: '#00b8d4', title: 'Carte 4' },
-    ])
+  const [cards, setCards] = useState([
+    { id: uuidv4(), color: '#5cdb95', title: 'Carte 1' },
+    { id: uuidv4(), color: '#f56991', title: 'Carte 2' },
+    { id: uuidv4(), color: '#f39c12', title: 'Carte 3' },
+    { id: uuidv4(), color: '#00b8d4', title: 'Carte 4' },
+  ]);
+
 
     const getTaskPos = id => cards.findIndex(card => card.id ===id)
 
@@ -46,7 +48,7 @@ export default function App() {
     const deleteCard = (cardId) => {
       setCards((prevCards) => prevCards.filter((card) => card.id !== cardId));
     };
-    
+
     return (
       <div className="App">
         <h1> Frise d'initiative</h1>
